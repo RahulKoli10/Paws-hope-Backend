116e7375977c810295b2fd7660422f0c11976857
RahulKoli10 <rahulkohli7078@gmail.com>
2026-05-21 12:59:32 +0530

    first commit

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,17 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL ="postgresql://neondb_owner:npg_HT1lJPXh2RrL@ep-damp-night-ap211q3k-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 Base = declarative_base()
 
 def get_db():
